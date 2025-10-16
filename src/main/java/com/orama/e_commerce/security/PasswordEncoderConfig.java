93258611074b807532dee594ed927b5d39ad4831
Thiagooms <thiagopaulista50@gmail.com>
2025-10-16 14:43:43 -0300

    [chore] structure: move folder

--- src/main/java/com/orama/e_commerce/security/PasswordEncoderConfig.java
+++ src/main/java/com/orama/e_commerce/security/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package com.orama.e_commerce.config;
+package com.orama.e_commerce.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
