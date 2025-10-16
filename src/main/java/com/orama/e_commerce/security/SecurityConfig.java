7e539a9b7803849af6f849ca43a2ed500ee6f61f
Thiagooms <thiagopaulista50@gmail.com>
2025-10-16 14:50:43 -0300

    [chore] move method: passes the encode responsibility to the correct method

--- src/main/java/com/orama/e_commerce/security/SecurityConfig.java
+++ src/main/java/com/orama/e_commerce/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -61,4 +62,9 @@ public class SecurityConfig {
       throws Exception {
     return config.getAuthenticationManager();
   }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
