96dc52910354c4fdbf24849ed6541c5d6d59ebb5
Thiagooms <thiagopaulista50@gmail.com>
2026-02-13 16:45:38 -0300

    [feature] forgot-password: add migration, entity, repository and DTOs

--- src/main/java/com/orama/e_commerce/dtos/auth/ForgotPasswordRequestDto.java
+++ src/main/java/com/orama/e_commerce/dtos/auth/ForgotPasswordRequestDto.java
@@ -4,4 +4,5 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record ForgotPasswordRequestDto(
-    @NotBlank(message = "Email is required") @Email(message = "Email must be valid") String email) {}
+    @NotBlank(message = "Email is required") @Email(message = "Email must be valid")
+        String email) {}
